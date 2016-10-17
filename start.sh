00ac2d2fc1b347cffe329652176816bc76754bc1
Soichi Hayashi <hayashis@iu.edu>
2016-10-17 13:22:08 -0400

    added clean.sh

--- start.sh
+++ start.sh
@@ -10,6 +10,9 @@
 #
 
 #allows test execution
+if [ -z $SCA_SERVICE_DIR ]; then
+    export SCA_SERVICE_DIR=`pwd`
+fi
 if [ -z "$SCA_PROGRESS_URL" ]; then
     export SCA_PROGRESS_URL="https://soichi7.ppa.iu.edu/api/progress/status/_sca.test"
 fi
