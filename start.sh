a6b6e3c45c4aedd8b1a62f7a517e5a1396f01fb0
Soichi Hayashi <hayashis@iu.edu>
2017-01-26 16:02:26 -0500

    removed jq install

--- start.sh
+++ start.sh
@@ -8,15 +8,6 @@ if [ -z "$SCA_PROGRESS_URL" ]; then
     export SCA_PROGRESS_URL="https://soichi7.ppa.iu.edu/api/progress/status/_sca.test"
 fi
 
-#make sure jq is installed on $SCA_SERVICE_DIR
-#if [ ! -f $SCA_SERVICE_DIR/jq ];
-#then
-#        echo "installing jq"
-#        wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O $SCA_SERVICE_DIR/jq
-#        chmod +x $SCA_SERVICE_DIR/jq
-#fi
-#
-
 #patch libssl issue caused by some module overriding libpath
 unset LD_LIBRARY_PATH
 
