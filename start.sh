a9a29d77c82bbb425de23a961754d3b3913b48f2
Ubuntu <hayashis@azure.rnh4gg0xnwqedkctxwqqlf5v5e.bx.internal.cloudapp.net>
2017-07-04 22:05:16 +0000

    updated to use locally staged life container - until i can figure out how to get on-the-fly conversion working with singularity

--- start.sh
+++ start.sh
@@ -17,7 +17,8 @@ if [ $ENV == "SINGULARITY" ]; then
     echo $! > pid
     
 cat <<EOT > _run.sh
-time singularity run docker://brainlife/life
+#time singularity run docker://brainlife/life
+time singularity run /usr/local/brainlife_life.img
 
 #check for output files
 if [ -s output_fe.mat ];
