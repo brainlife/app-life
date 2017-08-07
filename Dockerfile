5a5f13cb2cc5d786088db9b3ee87651059646cdc
Soichi Hayashi <hayashis@iu.edu>
2017-08-07 12:48:54 +0000

    added "RUN ldconfig" at the end of Dockerfile as suggested by singularity best practice

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,15 @@ FROM brainlife/mcr:centos6-r2016a
 MAINTAINER Soichi Hayashi <hayashis@iu.edu>
 
 #for openmp
-RUN yum update && yum -y install libgomp
+RUN yum -y update && yum install -y libgomp
 
 ADD /msa /msa
 
 #we want all output to go here (config.json should also go here)
 WORKDIR /output
 
+#http://singularity.lbl.gov/docs-docker#be-practices
+RUN ldconfig
+
 ENTRYPOINT ["/msa/main"] 
+
--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #mcc -m main -R -nodisplay -a /N/u/hayashis/BigRed2/git/encode-mexed/mexfiles -d msa
 docker build -t brainlife/life . && docker push brainlife/life
 
-echo "publishing image to hcp singularity /images directory"
-docker run \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v /usr/local/images:/output \
-	--privileged -t --rm \
-	singularityware/docker2singularity \
-	brainlife/life
+#echo "publishing image to hcp singularity /images directory"
+#docker run \
+#	-v /var/run/docker.sock:/var/run/docker.sock \
+#	-v /usr/local/images:/output \
+#	--privileged -t --rm \
+#	singularityware/docker2singularity \
+#	brainlife/life
 
 #-v /mnt/auto/soft/cle5/singularity/images:/output \
 
