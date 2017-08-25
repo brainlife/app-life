c085ec39db0aac2d4c4f90700b0fe88e309f5332
Soichi Hayashi <hayashis@iu.edu>
2017-08-25 01:07:03 +0000

    fixed Dockerfile for mcr2007

--- stop.sh
+++ stop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-jobid=`cat jobid`
-echo "running qdel $jobid"
-qdel $jobid
+if [ -f jobid ]; then
+	jobid=`cat jobid`
+	echo "running qdel $jobid"
+	qdel $jobid
+fi
+
+if [ -f pid ]; then
+	kill $(cat pid)
+fi
