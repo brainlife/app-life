10f59814666edb590f805ed5fe2b5a0458d141a8
Soichi Hayashi <hayashis@iu.edu>
2017-09-12 13:14:07 -0400

    recompiled with 2017a again

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-#mcc -m main -R -nodisplay -a /N/u/hayashis/BigRed2/git/encode-mexed/mexfiles -d msa
+#mcc -m main -R -nodisplay -a /N/u/hayashis/BigRed2/git/encode-mexed/mexfiles -d msa-r2017a
 docker build . -t brainlife/life && docker tag brainlife/life brainlife/life:1.1 && docker push brainlife/life
 
