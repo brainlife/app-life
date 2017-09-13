12b8b85389e6fca9368102dfca62e997a1029190
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-12 22:00:25 -0400

    updated to add weights to json and round output

--- main.m
+++ main.m
@@ -44,7 +44,7 @@ w = feGet(fe,'fiber weights');
 fg = fgExtract(fg, w > 0, 'keep');
 
 fg_sub = fg;
-fg_sub.fibers = fg.fibers(1:10:end,:);
+fg_sub.fibers = round(fg.fibers(1:10:end,:), 4);
 
 connectome.name = 'subsampled (x10) pos. weighted life output';
 connectome.coords = fg_sub.fibers;
