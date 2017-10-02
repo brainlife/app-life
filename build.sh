f42a79cf95ae11234a2e8a6560fa21c6addd4921
Soichi Hayashi <hayashis@iu.edu>
2017-10-02 17:08:55 -0400

    recompiled with latest change made to tractjson

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 
-#mcc -m main -R -nodisplay -a /N/u/hayashis/brlife/git/encode/mexfiles -d msa-r2017a
-docker build . -t brainlife/life && \
-	docker tag brainlife/life brainlife/life:1.2 && \
-	docker push brainlife/life
+mcc -m main -R -nodisplay -a /N/u/brlife/git/encode/mexfiles -d msa-r2017a
+
+#docker build . -t brainlife/life && \
+#	docker tag brainlife/life brainlife/life:1.2 && \
+#	docker push brainlife/life
 
