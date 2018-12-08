d8fe52491d7c0b56ecd9243cd2e575212a04583c
Soichi Hayashi <hayashis@iu.edu>
2018-12-08 00:23:54 -0500

    recomipled yet again

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
-mcc -m -R -nodisplay -a /N/u/brlife/git/encode/mexfiles -d compiled main
+mcc -g -m -R -nodisplay -a /N/u/brlife/git/encode/mexfiles -d compiled main
 exit
 END
 matlab -nodisplay -nosplash -r build && rm build.m
