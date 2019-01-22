5c78510ab2bd09c41a7815136d2b110e8a560ffe
Soichi Hayashi <hayashis@iu.edu>
2019-01-21 23:46:27 -0500

    finally fixed the issue of fibers gone missing

--- compile.sh
+++ compile.sh
@@ -4,13 +4,14 @@ module load matlab/2018a
 
 cat > build.m <<END
 
-% addpath(genpath('/N/u/hayashis/git/encode'))
+%addpath(genpath('/N/u/brlife/git/encode'))
+addpath(genpath('/N/u/hayashis/git/encode'))
 
-addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
-mcc -m -R -nodisplay -a /N/u/brlife/git/encode/mexfiles -d compiled main
+%mcc -m -R -nodisplay -a /N/u/brlife/git/encode/mexfiles -d compiled main
+mcc -m -R -nodisplay -a /N/u/hayashis/git/encode/mexfiles -d compiled main
 exit
 END
-matlab -nodisplay -nosplash -r build && rm build.m
+time matlab -nodisplay -nosplash -r build && rm build.m
 
