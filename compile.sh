95b2f027d499447eed10afc640503b71ef07e6f5
Soichi Hayashi <hayashis@iu.edu>
2019-08-15 17:41:09 -0400

    recompiled with my local vistasoft with tck loader memory issue patch

--- compile.sh
+++ compile.sh
@@ -5,8 +5,8 @@ module load matlab/2017a
 cat > build.m <<END
 
 addpath(genpath('/N/u/brlife/git/encode'))
-%addpath(genpath('/N/u/hayashis/git/encode'))
-addpath(genpath('/N/u/brlife/git/vistasoft'))
+%addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/hayashis/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 
 mcc -m -R -nodisplay -a /N/u/brlife/git/encode/mexfiles -d compiled main
